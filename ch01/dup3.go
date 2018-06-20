c9c7f955cb419e736ff9dc14cdb4f0a476054cb5
Tony ZHOU <zjt1114@gmail.com>
2018-06-19 23:00:30 -0700

    Add dup3.go

---
+++ ch01/dup3.go
@@ -0,0 +1,30 @@
+// +build dup3
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, c := range counts {
+		if c > 0 {
+			fmt.Printf("%s => %d\n", line, c)
+		}
+	}
+}
