8b1fd46999e509c89d68ccfbfe3671288c569fab
Tony ZHOU <zjt1114@gmail.com>
2018-06-24 20:31:54 -0700

    Add server3

--- ch01/lissajous.go
+++ ch01/lissajous.go
@@ -7,9 +7,10 @@ import (
 	"image/color"
 	"image/gif"
 	"io"
+	"log"
 	"math"
 	"math/rand"
-	"os"
+	"net/http"
 )
 
 var palette = []color.Color{color.Black, color.RGBA{99, 0, 0, 10}}
@@ -20,7 +21,11 @@ const (
 )
 
 func main() {
-	lissajous(os.Stdout)
+	// lissajous(os.Stdout)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		lissajous(w)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func lissajous(out io.Writer) {
